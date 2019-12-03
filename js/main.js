2760dfcd25b6fdeeae78ea30142858a067e6ff46
jhamadhav <jhamadhav28@gmail.com>
2019-12-03 14:57:54 +0530

    added control details

--- js/main.js
+++ js/main.js
@@ -22,8 +22,8 @@ window.addEventListener('keyup', function (e) {
 
 //with swipe
 /*  actual swipe capture */
-// window.addEventListener('touchstart', handleTouchStart, false);
-// window.addEventListener('touchmove', handleTouchMove, false);
+window.addEventListener('touchstart', handleTouchStart, false);
+window.addEventListener('touchmove', handleTouchMoveForNav, false);
 
 var xDown = null, yDown = null;
 
@@ -73,6 +73,30 @@ function handleTouchMove(evt) {
         }
     }
 
+    /* reset values */
+    xDown = null; yDown = null;
+};
+
+//for nav
+function handleTouchMoveForNav(evt) {
+    evt.preventDefault();
+    if (!xDown || !yDown) { return; }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+
+        /*Response part*/
+        if (xDiff > 0) {
+            /* right swipe */
+            menu();
+        } else {
+            menu();
+        }
+    }
     /* reset values */
     xDown = null; yDown = null;
 };
