abc725425fb5fb04087ab0cd557dd9a02be7269b
jhamadhav <jhamadhav28@gmail.com>
2019-12-04 22:37:43 +0530

    nav bar fixed

--- navbar/nav.js
+++ navbar/nav.js
@@ -10,12 +10,16 @@ let prev, now;
 window.onscroll = function () {
     now = window.pageYOffset;
     var btn = document.getElementsByTagName("nav")[0];
-    if (now > 70) {
+    var rect = document.getElementsByClassName("nav-links")[0].getBoundingClientRect();
+    // console.log(rect.top, rect.right, rect.bottom, rect.left);
+    if (now > 70 && rect.left < 0) {
         if (now - prev < 0) {
             btn.style.transform = "translateY(0)";
         } else {
             btn.style.transform = "translateY(-70px)";
         }
+    } else {
+        btn.style.transform = "translateY(0)";
     }
     prev = window.pageYOffset;
 };
