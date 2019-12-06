5a2be908ec14837198bb488d0e96ea5b16cf4a66
jhamadhav <jhamadhav28@gmail.com>
2019-12-06 21:42:58 +0530

    threshold changed v2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 var tile, dir = null;
 var tile_set = [];
 var score, best = 0, count_hint = 0;
-const swipeThreshold = 2;
+const swipeThreshold = 0.5;
 //some in game tips
 const hints = [
     'Click best to reset it !',
