aca24712487f61b61e0f854d27358a77b8366083
jhamadhav <jhamadhav28@gmail.com>
2020-03-16 16:08:18 +0530

    pwa made offline

--- SW.js
+++ SW.js
@@ -1,4 +1,4 @@
-const cacheName = 'v2';
+const cacheName = 'v3';
 const assets = [
     './index.html',
     './css/main.css',
@@ -25,17 +25,20 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches
             .open(cacheName)
-            .then(cache => {
+            .then((cache) => {
                 console.log('Service Worker: Caching File');
-                cache.addAll(assets);
+                return cache.addAll(assets);
             })
-            .then(() => self.skipWaiting())
+            .then(() => { self.skipWaiting(); })
     );
 });
 
 // call activate event
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activated');
+self.addEventListener("activate", function (event) {
+
+    // Allow sw to control of current page
+    console.log("Claiming clients for current page");
+    event.waitUntil(self.clients.claim());
 
     // remove unwanted previously cached files
     event.waitUntil(
@@ -53,12 +56,66 @@ self.addEventListener('activate', (event) => {
     );
 });
 
-// call fetch event
-self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching');
-    // first check if live site is available else fetch file from cache
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) {
+    if (event.request.method !== "GET") return;
+
     event.respondWith(
-        /* if there is no connection then fetching will fail then we would call a catch function since it returns a promise*/
-        fetch(event.request).catch(() => caches.match(e.request))
-    )
-})
+        fromCache(event.request)
+            .then(
+                function (response) {
+                    // The response was found in the cache so we respond with it and update the entry
+
+                    // This is where we call the server to get the newest version of the
+                    // file to use the next time we show view
+                    event.waitUntil(
+                        fetch(event.request)
+                            .then(function (response) {
+                                console.log('Updating Files: ', response);
+                                return updateCache(event.request, response);
+                            })
+                            .catch(function (err) {
+                                console.log('No network Connection: Failed to update Files', err);
+                            })
+
+                    );
+
+                    return response;
+                },
+                function () {
+                    // The response was not found in the cache so we look for it on the server
+                    return fetch(event.request)
+                        .then(function (response) {
+                            // If request was success, add or update it in the cache
+                            event.waitUntil(updateCache(event.request, response.clone()));
+
+                            return response;
+                        })
+                        .catch(function (error) {
+                            console.log("Network request failed and no cache." + error);
+                        });
+                }
+            )
+    );
+});
+
+function fromCache(request) {
+    // Check to see if you have it in the cache
+    // Return response
+    // If not in the cache, then return
+    return caches.open(cacheName).then(function (cache) {
+        return cache.match(request).then(function (matching) {
+            if (!matching || matching.status === 404) {
+                return Promise.reject("no-match");
+            }
+
+            return matching;
+        });
+    });
+}
+
+function updateCache(request, response) {
+    return caches.open(cacheName).then(function (cache) {
+        return cache.put(request, response);
+    });
+}
